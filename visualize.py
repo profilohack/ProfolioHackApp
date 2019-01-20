0ddd0a717ff899b20329b7e6c15a284a91dcb6f4
NatashaTing <zetpingting@gmail.com>
2019-01-20 09:04:14 -0700

    add pie chart html

--- visualize.py
+++ visualize.py
@@ -1,4 +1,10 @@
 import plotly
+
+import plotly.plotly as py
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+plotly.tools.set_credentials_file(username='NatashaTing', api_key='D2EsU928WZxYqs9NRPdF')
+plotly.tools.set_config_file(world_readable=True,
+                             sharing='public')
 import plotly.graph_objs as go
 import plotly.tools as tls
 import cufflinks as cf
@@ -8,8 +14,14 @@ import getData
 
 def vis(df, date, returns):
     print(plotly.__version__)
-    df = cf.datagen.lines()
-    df.iplot(kind='scatter', filename='cufflinks/cf-simple-line')
+    spendingcategory = date
+    amount = returns
+    spendingcategory = ['Boat supply', 'eTransfer - Vacation Property rental Total', 'Hotels & Motels Total',
+            'Retail - Books & Household items Total','Services - Marketing Total']
+    amount = [307.47,45000,2774.79,18867.93,4234.78]
+
+    trace = go.Pie(labels=spendingcategory, values=amount)
+    py.iplot([trace], filename='batman_pie_chart')
 
 
 def getSeries():
