fc601d2eae0d600efd5902c6a7470789c125f309
NatashaTing <zetpingting@gmail.com>
2019-01-20 09:52:30 -0700

    small error

--- getData.py
+++ getData.py
@@ -12,12 +12,8 @@ def getAllFiles():
     filelist = glob.glob("csv/"+"*.csv")
         # print(file)
     out = [getData(file) for file in filelist]
-<<<<<<< HEAD
-=======
     # print(out)
->>>>>>> 1abcd333db6ba286203a1dabb8a40449f6d850da
     return out
-
 
 #getAllFiles()
 getData("csv/IGGBR10D.csv")
