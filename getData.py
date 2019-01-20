511c03c4517356e8ad97ec0b6c81b3a9c3a03ed9
NatashaTing <zetpingting@gmail.com>
2019-01-20 05:05:18 -0700

    processed csv and added getData module

---
+++ getData.py
@@ -0,0 +1,17 @@
+import csv
+import pandas as pd
+import glob
+
+def getData(filename):
+    #df = pd.read_csv(filename, parse_dates=["Date"], skiprows=2)
+    df = pd.read_csv(filename, parse_dates=["Date"])
+    df = df.iloc[:, [0, -1]]
+
+def getAllFiles():
+    filelist = glob.glob("csv/"+"*.csv")
+    for file in filelist:
+        print(file)
+        getData(file)
+
+getAllFiles()
+#getData("csv/IGGBR10D.csv")
