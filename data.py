3b6767d62780e79a6c5f96de3165a910149cd4e5
dfloren <daniel_florendo1@yahoo.com>
2019-01-20 03:18:48 -0700

    Add Flask files

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Dummy Text Dummy Text Dummy Text Dummy Text',
+            'author': 'Name of Author here',
+            'create date': '01-19-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Dummy Text Dummy Text Dummy Text Dummy Text',
+            'author': 'Name of Author here',
+            'create date': '01-19-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Dummy Text Dummy Text Dummy Text Dummy Text',
+            'author': 'Name of Author here',
+            'create date': '01-19-2019'   
+        }
+    ]
+    return articles
